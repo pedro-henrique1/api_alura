5268e54753430dea8544ad6f6fbe6732ec14e144
Julien Bisconti <julienbi@unity3d.com>
2022-02-05 17:19:50 -0300

    criação das seeds e das models

---
+++ database/factories/RecipeFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use JetBrains\PhpStorm\ArrayShape;
+
+class RecipeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     * @throws \Exception
+     */
+    #[ArrayShape(['description' => "string", 'valor' => "int", 'data' => "string"])] public function definition()
+    {
+        return [
+            'description' => $this->faker->text(100),
+            'valor' => random_int(1000, 9999),
+            'data' => $this->faker->date
+        ];
+    }
+}
